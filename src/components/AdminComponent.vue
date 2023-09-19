58efb4bd49485d71de9b050a483aeff2ab79558d
prathyush-cubet <prathyush.lal@cubettech.com>
2023-09-19 09:22:34 +0530

    progress

--- src/components/AdminComponent.vue
+++ src/components/AdminComponent.vue
@@ -1,24 +1,32 @@
 <template>
     <div class="container">
+        <tr><td><input type="number" class="form-control" placeholder="No.of Questions" v-model="no_of_elements" @keyup="addQuestion(no_of_elements)"/></td></tr>
         <table v-for="(control, index) in form_controls" :key="index">
-          <tr><td><input type="number" class="form-control" placeholder="No.of Questions" v-model="control.no_of_elements"/></td></tr>
-          <tr><td><input type="text" class="form-control" placeholder="Subject" v-model="control.subject"/></td></tr>
-          <tr><td><input type="number" class="form-control"  placeholder="Options count" v-model="control.option_count" @keyup="addOptions(control)"/></td></tr>
-          <tr v-if="control.options.length"><td>
+          <tr><td>
             <div class="p-3">
               <table>
-                <tr v-for="(option,index) in control.options" :key="index"><td>
-                  <table>
-                    <tr v-for="(index, option) in option" :key="index">
-                      <td>
-                      <input type="text" class="form-control"  placeholder="Options" v-model="option.value"/>
-                      </td>
-                    </tr>
-                  </table>
+                <tr><td><input type="text" class="form-control" placeholder="Subject" v-model="control.subject"/></td></tr>
+                <tr><td><input type="number" class="form-control"  placeholder="Options count" v-model="control.option_count" @keyup="addOptions(control)"/></td></tr>
+                <tr v-if="control.options.length"><td>
+                  <div class="p-3">
+                    <table>
+                      <tr v-for="(option,index) in control.options" :key="index"><td>
+                        <table>
+                          <tr v-for="(index, option) in option" :key="index">
+                            <td>
+                            <input type="text" class="form-control"  placeholder="Options" v-model="option.value"/>
+                            </td>
+                          </tr>
+                        </table>
+                      </td></tr>
+                    </table>
+                  </div>
                 </td></tr>
               </table>
+              
             </div>
           </td></tr>
+          
         </table>
     </div>
 </template>
@@ -28,10 +36,10 @@ export default {
   name: 'AdminComponent',
   data() {
     return {
+      no_of_elements: 1,
       form_controls: [
         {
           id : 1,
-          no_of_elements: 1,
           option_count: 1,
           subject: '',
           options: [
@@ -46,12 +54,27 @@ export default {
   methods : {
     addOptions(control) 
     {
-      for(var i=1; i<control.option_count;i++) {
+      for(var i=2; i<=control.option_count;i++) {
         control.options.push({
         'value' : 'test'
       });
       }
       
+    },
+    addQuestion(no_of_elements) {
+      this.form_controls = [];
+      for(var i=1; i<=no_of_elements;i++) {
+        this.form_controls.push({
+          id : i,
+          option_count: 1,
+          subject: '',
+          options: [
+            {
+              'value' : 'test'
+            }
+          ]
+        });
+      }
     }
   }
 }
